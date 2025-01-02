ce261c92dc6165f4898489a1293c3401c028e150
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-02 09:21:58 -0300

    :sparkles: feat: define User entity with id, name, email, and password fields

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+}
