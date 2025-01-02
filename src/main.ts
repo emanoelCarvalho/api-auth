f05bd1065e6fa48a535d2b3e3a29c3e60d35094c
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-02 10:46:14 -0300

    :bug: fix: Update server port configuration to use dynamic port from AppConfigService

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(AppConfigService);
   const port = configService.getPort; 
-  console.log(`Server is running on port ${port}`);
-  await app.listen(3000);
+
+  await app.listen(port);
 }
 bootstrap();
