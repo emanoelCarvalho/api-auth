954ebabc9273aae0d4930e4c7faf3407593a48b2
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-03 18:34:28 -0300

    :bug: fix: Remove unused imports from app.controller.ts and app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { AppConfigModule } from './app-config/app-config.module';
-import { AppConfigService } from './app-config/app-config.service';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
