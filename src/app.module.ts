dcd824a07f1b928570ccd270b58565ebff6b1aaf
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-08 22:54:23 -0300

    :boom: fix: Remove unused CriptoModule import from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,9 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { AppConfigModule } from './app-config/app-config.module';
 import { DatabaseModule } from './database/database.module';
-import { CriptoModule } from './cripto/cripto.module';
 
 @Module({
-  imports: [
-    AppConfigModule,
-    UsersModule,
-    AuthModule,
-    DatabaseModule,
-    CriptoModule,
-  ],
+  imports: [AppConfigModule, UsersModule, AuthModule, DatabaseModule],
   controllers: [AppController],
 })
 export class AppModule {}
