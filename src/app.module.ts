b6bc40dbcd02a2e621fdfa5baa971b073804f355
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-14 00:02:14 -0300

    :fire: chore: Remove AppController from the application module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { AppConfigModule } from './app-config/app-config.module';
@@ -7,6 +6,5 @@ import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [AppConfigModule, UsersModule, AuthModule, DatabaseModule],
-  controllers: [AppController],
 })
 export class AppModule {}
