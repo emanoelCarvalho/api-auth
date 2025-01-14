ed7f68b25379d2198d7b5dea25f72a60bc30a6cc
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-14 00:05:23 -0300

    :fire: chore: Remove Swagger setup from main.ts and update port variable naming

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './app-config/app-config.service';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(AppConfigService);
-  const config = new DocumentBuilder()
-  .setTitle('API auth')
-  .setDescription('Documentação da API de autenticação')
-  .setVersion('1.0')
-  .addBearerAuth()
-  .build();
-  const document = SwaggerModule.createDocument(app, config); 
-  SwaggerModule.setup('api', app, document);
-  const port = configService.getPort; 
+  const PORT = configService.getPort; 
 
-  await app.listen(port);
+  await app.listen(PORT);
 }
 bootstrap();
