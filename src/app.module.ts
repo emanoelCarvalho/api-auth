6e04ee65d4f60e9c1625007dfd6fbddcddfa6b30
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-14 00:03:56 -0300

    :sparkles: feat: Rename SwaggerModule to SwaggerModuleConfig and update imports in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { AppConfigModule } from './app-config/app-config.module';
 import { DatabaseModule } from './database/database.module';
-import { SwaggerModule } from './swagger/swagger.module';
+import { SwaggerModuleConfig } from './swagger/swagger.module';
 
 @Module({
-  imports: [AppConfigModule, UsersModule, AuthModule, DatabaseModule, SwaggerModule],
+  imports: [
+    AppConfigModule,
+    UsersModule,
+    AuthModule,
+    DatabaseModule,
+    SwaggerModuleConfig,
+  ],
 })
 export class AppModule {}
