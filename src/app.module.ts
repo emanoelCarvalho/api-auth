184f61932063b30a53947128b123f5b66d7d9bd2
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-14 00:03:13 -0300

    :sparkles: feat: Add Swagger module to the application for API documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { AppConfigModule } from './app-config/app-config.module';
 import { DatabaseModule } from './database/database.module';
+import { SwaggerModule } from './swagger/swagger.module';
 
 @Module({
-  imports: [AppConfigModule, UsersModule, AuthModule, DatabaseModule],
+  imports: [AppConfigModule, UsersModule, AuthModule, DatabaseModule, SwaggerModule],
 })
 export class AppModule {}
