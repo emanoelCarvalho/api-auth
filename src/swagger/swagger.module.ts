353c0bdaad9fb18f12b43454dae5f757af2a6e76
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-14 00:07:17 -0300

    :sparkles: feat: Implement Swagger setup method in SwaggerModuleConfig for API documentation

--- src/swagger/swagger.module.ts
+++ src/swagger/swagger.module.ts
@@ -1,4 +1,17 @@
-import { Module } from '@nestjs/common';
+import { INestApplication, Module } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 @Module({})
-export class SwaggerModuleConfig {}
+export class SwaggerModuleConfig {
+  static setup(app: INestApplication): void {
+    const config = new DocumentBuilder()
+      .setTitle('API auth')
+      .setDescription('Documentação da API de autenticação')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
+}
